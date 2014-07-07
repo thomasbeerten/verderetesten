4bb9a6417e56449fed38ae02efcebd2e8c928b9d
thomasbeerten <thomas_beerten@hotmail.com>
2014-07-07 11:41:01 +0200

    panningview implemented samen met nineoldandroids lib

--- app/src/main/java/com/thomascbeerten/verderetesten/app/FragmentPage1.java
+++ app/src/main/java/com/thomascbeerten/verderetesten/app/FragmentPage1.java
@@ -4,12 +4,18 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.fourmob.panningview.PanningView;
+
 
 public class FragmentPage1 extends Fragment {
+
+    PanningView panningView;
+
     public FragmentPage1() {
         // Required empty public constructor
     }
@@ -17,7 +23,21 @@ public class FragmentPage1 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_fragment_page1, container, false);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_page1, container, false);
+        panningView = (PanningView) v.findViewById(R.id.panningView);
+        if (panningView != null) {
+            panningView.startPanning();
+            Log.d("panningview", "panningview not null");
+        } else {
+            Log.d("panningview", "panningview null");
+        }
+        return v;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 }
